1a9a2911bfe2bc9df44eff44cbf3ce014891a9bf
Warelephante <filippramsey@icloud.com>
2026-05-08 13:55:32 -0400

    Update main.js

--- game_i2/client/main.js
+++ game_i2/client/main.js
@@ -2,7 +2,7 @@ import Phaser from "phaser";
 
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000");
+const socket = io("https://star-s58j.onrender.com");
 
 socket.on("connect", () => {
   socket.emit("test", "hello from client");
